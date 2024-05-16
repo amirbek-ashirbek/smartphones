3a9463aa83e43eddb39b735873c5b286b5d102bc
amirbek-ashirbek <amirbek.ashirbek@gmail.com>
2024-05-17 02:51:40 +0500

    Added displaying loading in images and made app portrait mode only

--- app/src/main/java/com/example/mechtasmartphones/feature_catalog/domain/model/product/ProductType.kt
+++
@@ -1,5 +0,0 @@
-package com.example.mechtasmartphones.feature_catalog.domain.model.product
-
-enum class ProductType {
-	SMARTPHONE
-}
--- app/src/main/java/com/example/mechtasmartphones/feature_catalog/presentation/components/ProductImage.kt
+++ app/src/main/java/com/example/mechtasmartphones/feature_catalog/presentation/components/ProductImage.kt
@@ -3,8 +3,9 @@ package com.example.mechtasmartphones.feature_catalog.presentation.components
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import coil.compose.AsyncImage
+import coil.compose.SubcomposeAsyncImage
 import coil.request.ImageRequest
+import com.example.mechtasmartphones.core.presentation.components.CustomLoadingIndicator
 import com.example.mechtasmartphones.feature_catalog.presentation.util.Constants
 
 @Composable
@@ -12,13 +13,16 @@ fun ProductImage(
 	url: String,
 	modifier: Modifier = Modifier
 ) {
-	AsyncImage(
+	SubcomposeAsyncImage(
 		model = ImageRequest
 			.Builder(LocalContext.current)
 			.data(url)
 			.crossfade(durationMillis = Constants.PRODUCT_ITEM_CROSSFADE_ANIMATION_DURATION_IN_MILLIS)
 			.build(),
 		contentDescription = null,
+		loading = {
+			CustomLoadingIndicator()
+		},
 		modifier = modifier
 	)
 }
